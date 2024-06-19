ac289c816f6d8cef4de1aaf0dd2dab82dc5ab16e
xuabcuong <xuabcuong@gmail.com>
2024-06-20 01:39:18 +0700

    push code

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'webbanhang_wp' );
+define( 'DB_NAME', 'webtest' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
